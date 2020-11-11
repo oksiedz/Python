96a2e86155a2de0d4454de1e2b654ebb2e287ce3
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2020-11-11 21:07:22 +0100

    Przerobienie kodu trunieju tak, żeby zwracał maksymalną liczbę i drugą w kolejności

--- Projects/Project1/brudnopis.py
+++ Projects/Project1/brudnopis.py
@@ -2,44 +2,50 @@ import random
 import datetime
 import math
 
-NoOfNumbers = 10
-#NoOfNumbers = int(input('Podaj dlugosc ciagu liczb: '))
+NoOfNumbers = 5
 randomlist = []
-print("Rozpoczecie generowania ciagu liczb.")
+#start of data generation
 for i in range(0, NoOfNumbers):
     randomlist.append(random.random())
     print(randomlist[i])
 print("Zakonczenie generowania ciagu liczb.")
+#end of data generation
 
-#ogarnąć to w pętli while z brake gdy liczba winnerów będzie równa 1 - to jest max
-
+#start clearing and defining parameters
 listofpairs = []
 listofloserpairs = []
 listofnumbers = randomlist
 koniec_szukania = 0
 listofwinners = []
 change_the_listofnumbers = 0
+round = 1
+#end clearing and defining parameters
+
+#start of loop
 while (koniec_szukania == 0):
-    
     if change_the_listofnumbers == 1:
         listofnumbers = listofwinners
         listofwinners = []
-    #podział na pary
     n = int(math.ceil(len(listofnumbers) / 2))
     print ("liczba par", n)
     
     if n == 1:
         koniec_szukania = 1
         if float(listofnumbers[0]) > float(listofnumbers[1]):
-            max_1 = listofnumbers[0]
+            if round == 1:
+                max_1 = listofnumbers[0]
+            if round == 2:
+                max_2 = listofnumbers[0]
             listofloserpairs.append(str(listofnumbers[0])+";"+str(listofnumbers[1]))
         else:
-            max_1 = listofnumbers[1]
+            if round == 1:
+                max_1 = listofnumbers[1]
+            if round == 2:
+                max_2 = listofnumbers[1]
             listofloserpairs.append(str(listofnumbers[1])+";"+str(listofnumbers[0]))
     else:
         for i in range(0, n):
             if 2*i+1<len(listofnumbers):
-                #listofpairs.append(str(listofnumbers[2*i]) + ";" + str(listofnumbers[2*i+1]))
                 if listofnumbers[2*i] > listofnumbers[2*i+1]:
                     listofwinners.append(listofnumbers[2*i])
                     listofloserpairs.append(str(listofnumbers[2*i])+";"+str(listofnumbers[2*i+1]))
@@ -47,27 +53,68 @@ while (koniec_szukania == 0):
                     listofwinners.append(listofnumbers[2*i+1])
                     listofloserpairs.append(str(listofnumbers[2*i+1])+";"+str(listofnumbers[2*i]))
             else:
-                #listofpairs.append(str(listofnumbers[2*i])+";")
                 listofwinners.append(str(listofnumbers[2*i]))
-    print("Lista par które przegrały w pętli")
-    for i in range(0, len(listofloserpairs)):
-        print(listofloserpairs[i])
-
-    print("lista wygranych w pętli")
-    for i in range(0, len(listofwinners)):
-        print(listofwinners[i])
-
-
     change_the_listofnumbers = 1
+#end of loop
 
-print("Lista par które przegrały")
+#start clear the parameter to provide new players for the tournament
+listofnumbers = []
+#end clear the parameter to provide new players for the tournament
+
+#start creation of new players
 for i in range(0, len(listofloserpairs)):
-    print(listofloserpairs[i])
+    pairforcomparison = listofloserpairs[i].split(";")
+    if float(pairforcomparison[0]) == float(max_1):
+        listofnumbers.append(pairforcomparison[1])
 
-print("lista wygranych")
-for i in range(0, len(listofwinners)):
-    print(listofwinners[i])
+
+####start of second tournament for the losers with the winner
+listofpairs = []
+listofloserpairs = []
+koniec_szukania = 0
+listofwinners = []
+change_the_listofnumbers = 0
+round = 2
+#start of loop
+while (koniec_szukania == 0):
+    if change_the_listofnumbers == 1:
+        listofnumbers = listofwinners
+        listofwinners = []
+    n = int(math.ceil(len(listofnumbers) / 2))
+    print ("liczba par", n)
+    
+    if n == 1:
+        koniec_szukania = 1
+        if float(listofnumbers[0]) > float(listofnumbers[1]):
+            if round == 1:
+                max_1 = listofnumbers[0]
+            if round == 2:
+                max_2 = listofnumbers[0]
+            listofloserpairs.append(str(listofnumbers[0])+";"+str(listofnumbers[1]))
+        else:
+            if round == 1:
+                max_1 = listofnumbers[1]
+            if round == 2:
+                max_2 = listofnumbers[1]
+            listofloserpairs.append(str(listofnumbers[1])+";"+str(listofnumbers[0]))
+    else:
+        for i in range(0, n):
+            if 2*i+1<len(listofnumbers):
+                if listofnumbers[2*i] > listofnumbers[2*i+1]:
+                    listofwinners.append(listofnumbers[2*i])
+                    listofloserpairs.append(str(listofnumbers[2*i])+";"+str(listofnumbers[2*i+1]))
+                else:
+                    listofwinners.append(listofnumbers[2*i+1])
+                    listofloserpairs.append(str(listofnumbers[2*i+1])+";"+str(listofnumbers[2*i]))
+            else:
+                listofwinners.append(str(listofnumbers[2*i]))
+    change_the_listofnumbers = 1
+#end of loop
 
 
 print("Maksymalna liczba to:", max_1)
- 
+print("Druga maksymalna liczba to:", max_2)
+
+
+
+#def tournament()
