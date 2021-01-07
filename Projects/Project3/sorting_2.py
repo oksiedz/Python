bd70ae64a189a015fa4c1a016757c41a4efcc90e
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2021-01-07 16:27:52 +0100

    Reorganizaction of the code input array not changed (prevoisly was sorted) Added merge sort code

--- Projects/Project3/sorting_2.py
+++ Projects/Project3/sorting_2.py
@@ -1,5 +1,8 @@
 import random
+import datetime
 
+
+##############################Quick sort code
 def quicksort(arr, start , stop, mode):
 	if(start < stop):
 		if mode == 1: #Random pivot
@@ -15,7 +18,7 @@ def partitionrand(arr , start, stop):
 	randpivot = random.randrange(start, stop)
 
 	arr[start], arr[randpivot] = arr[randpivot], arr[start]
-	return partition(arr, start, stop)
+	return partitionlow(arr, start, stop)
 
 def partitionlow(arr,start,stop):
 	pivot = start
@@ -39,10 +42,95 @@ def partitionhigh(arr, start, stop):
 	arr[i+1], arr[stop] = arr[stop], arr[i+1]
 	return (i+1) 
 
+def quicksortengine(inputArray, quicksortmode = 1, inputType = "Z", ifSave = 0, saveResults = 0, measurePoint = 0):
+    A = []
+    for i in range(0, len(inputArray)):
+        A.append(inputArray[i])
+    startTime = datetime.datetime.now()
+    quicksort(inputArray, 0, len(A) - 1,quicksortmode)
+    endTime = datetime.datetime.now()
+    if (ifSave == 1):
+        for i in range(len(inputArray)):
+            sortedArrayAsc.append(inputArray[i])
+    if (saveResults == 1):
+        resultsList.append("Q;"+str(quicksortmode)+";"+str(inputType)+";"+str(measurePoint)+";"+str(endTime-startTime))
 
-array = [10, 7, 8, 9, 1, 5]
-print("Input array:")
-print(array)
-quicksort(array, 0, len(array) - 1, 3)
-print("Sorted array:")
-print(array)
+##############################Merge Sort code
+def mergeSort(alist):
+	if len(alist)>1:
+		mid = len(alist)//2
+		lefthalf = alist[:mid]
+		righthalf = alist[mid:]
+
+		mergeSort(lefthalf)
+		mergeSort(righthalf)
+
+		i=0
+		j=0
+		k=0
+		while i < len(lefthalf) and j < len(righthalf):
+			if lefthalf[i] <= righthalf[j]:
+				alist[k]=lefthalf[i]
+				i=i+1
+			else:
+				alist[k]=righthalf[j]
+				j=j+1
+			k=k+1
+
+		while i < len(lefthalf):
+			alist[k]=lefthalf[i]
+			i=i+1
+			k=k+1
+
+		while j < len(righthalf):
+			alist[k]=righthalf[j]
+			j=j+1
+			k=k+1
+
+def mergeSortEngine(inputArray, inputType = "Z", ifSave = 0, saveResults = 0, measurePoint = 0):
+	A = []
+	for i in range(0, len(inputArray)):
+		A.append(inputArray[i])
+	#print("Start - Merge sort")
+	startTime = datetime.datetime.now()
+	mergeSort(A)
+	endTime = datetime.datetime.now()
+	#print("End - Merge sort")
+	if (ifSave == 1):
+		for i in range(len(A)):
+			sortedArrayAsc.append(A[i])
+	if (saveResults == 1):
+		resultsList.append("M;"+str(inputType)+";"+str(measurePoint)+";"+str(endTime-startTime))
+
+##############################Heap
+
+
+################################Working code
+resultsList = []
+sortedArrayAsc = []
+sortedArrayDesc = []
+input = []
+randomArray = []
+noOfGeneratedNumber = 10
+
+print("Start of random array generation")
+for i in range(0, noOfGeneratedNumber):
+	randomArray.append(float(random.random()))
+print("End of random array generation")
+
+
+for i in range(0, len(randomArray)):
+        input.append(randomArray[i])
+
+print ("Input array is:")
+print(randomArray)
+
+#quicksortengine(inputArray = input, quicksortmode = 3, inputType = "R", ifSave = 1, saveResults = 1, measurePoint = 0)
+mergeSortEngine(inputArray = input, inputType = "R", ifSave = 1, saveResults = 1, measurePoint = 0)
+print ("Sorted array is:")
+print(sortedArrayAsc)
+print(randomArray)
+
+print ("Results:")
+print(resultsList)
+
